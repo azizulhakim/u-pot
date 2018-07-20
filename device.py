5698ed1a05090576ce49f64f0380fbd47434dc20
Azizul Hakim <azizulfahim2002@gmail.com>
2018-07-20 03:17:59 -0400

    generate full 'C' source code and add samsung smart tv in device description

--- device.py
+++ device.py
@@ -30,12 +30,12 @@ class Device(object):
         self.modelURL = root.find("./device/modelURL").text
         self.serialNumber = root.find("./device/serialNumber").text
         self.udn = root.find("./device/UDN").text
-        self.upc = root.find("./device/UPC").text
-        self.macAddress = root.find("./device/macAddress").text
-        self.firmwareVersion = root.find("./device/firmwareVersion").text
-        self.iconVersion = root.find("./device/iconVersion").text
-        self.binaryState = root.find("./device/binaryState").text
-        self.presentationURL = root.find("./device/presentationURL").text
+        #self.upc = root.find("./device/UPC").text
+        #self.macAddress = root.find("./device/macAddress").text
+        #self.firmwareVersion = root.find("./device/firmwareVersion").text
+        #self.iconVersion = root.find("./device/iconVersion").text
+        #self.binaryState = root.find("./device/binaryState").text
+        #self.presentationURL = root.find("./device/presentationURL").text
 
         self.iconList = []
         for icon in root.findall("./device/iconList/icon"):
@@ -59,9 +59,17 @@ class Device(object):
         return serviceList
 
     def generateServiceCode(self):
+        print("#include <libgupnp/gupnp.h>")
+        print("#include <stdlib.h>")
+        print("#include <gmodule.h>")
+        print("#include <locale.h>")
+        print("")
+
         for service in self.serviceList:
             for statevariable, value in service.stateVariables.items():
-                print("char " + statevariable + "[500] = \"" + value + "\";")
+                valueLength = str(len(value) + 5)
+                print("char " + statevariable + "[" + valueLength + "] = \"" + value + "\";")
+        print("")
 
         for service in self.serviceList:
             for action in service.actions:
@@ -101,18 +109,79 @@ class Device(object):
                 print('    gupnp_service_action_return (action);')
                 print("}\n")
 
+
+        # make main function now
+        print("int \nmain(G_GNUC_UNUSED int argc, G_GNUC_UNUSED char **argv)")
+        print("{")
+        print("  GOptionContext *optionContext;")
+        print("  GMainLoop *main_loop;")
+        print("  GError *error = NULL;")
+        print("  GUPnPContext *context;")
+        print("  GUPnPRootDevice *upnpDevice;")
         for service in self.serviceList:
-            out = '    GUPnPServiceInfo *{}Service;'
+            out = '  GUPnPServiceInfo *{}Service;'
             print(out.format(service.name))
 
+        print("")
+
+        print("  printf(\"%d\\n\", argc);")
+        print("  if (argc != 3) {")
+        print("    printf(\"Usage: ./light-server INTERFACE PORT\\n\");")
+        print("    exit(1);")
+        print("  }")
+        print("")
+        print("  char* interfacename = argv[1];")
+        print("  int port = atoi(argv[2]);")
+
+        print("  setlocale(LC_ALL, \"en_US.utf8\");")
+
+        print("  GString *gs = g_string_new(\"abc\");")
+        print("  g_print(\"%s\\n\", gs->str);")
+
+        print("\n\n\n")
+        print("  /* Create the UPnP context */")
+        print("  context = gupnp_context_new (interfacename, port, &error);")
+        print("  if (error) {")
+        print("    g_printerr (\"Error creating the GUPnP context: %s\\n\",")
+        print("    error->message);")
+        print("    g_error_free (error);")
+        print("    return EXIT_FAILURE;")
+        print("  }")
+
+        print("  //////// UPnP Device Start ///////////////////////////")
+        print("  upnpDevice = gupnp_root_device_new (context, \"setup.xml\", \".\", &error);")
+        print("  if (error != NULL) {")
+        print("    g_printerr (\"Error creating the GUPnP root device: %s\\n\",")
+        print("            error->message);")
+
+        print("    g_error_free (error);")
+
+        print("    return EXIT_FAILURE;")
+        print("  }")
+        print("  gupnp_root_device_set_available (upnpDevice, TRUE);")
+
+        print("")
         for service in self.serviceList:
-            out = '    {}Service = gupnp_device_info_get_service(GUPNP_DEVICE_INFO (belkindevice), "{}");'
+            out = '  {}Service = gupnp_device_info_get_service(GUPNP_DEVICE_INFO (upnpDevice), "{}");'
             print(out.format(service.name, service.serviceType))
 
-        print('-------------------------------------------------------------------------------------')
+        print('  -------------------------------------------------------------------------------------')
 
         for service in self.serviceList:
             for action in service.actions:
                 name = action.name
-                out = 'g_signal_connect(GUPNP_SERVICE({}Service), "action-invoked::{}", G_CALLBACK({}_cb), NULL);'
+                out = '  g_signal_connect(GUPNP_SERVICE({}Service), "action-invoked::{}", G_CALLBACK({}_cb), NULL);'
                 print(out.format(service.name, name, name))
+
+        print("")
+        print("  /* Run the main loop */")
+        print("  main_loop = g_main_loop_new (NULL, FALSE);")
+        print("  g_main_loop_run (main_loop);")
+
+        print("  /* Cleanup */")
+        print("  g_main_loop_unref (main_loop);")
+        print("  //  g_object_unref (service);")
+        print("  g_object_unref (context);")
+
+        print("  return EXIT_SUCCESS;")
+        print("}")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from device import Device
 
-device = Device('http://10.0.0.157:49153', '/setup.xml')
+#device = Device('http://10.0.0.157:49153', '/setup.xml')
+device = Device('http://10.0.0.141:9197', '/dmr')

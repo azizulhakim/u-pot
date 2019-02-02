1bbf0051c534508c1eab814b82018d1fbe587b9d
Azizul Hakim <azizulfahim2002@gmail.com>
2019-02-01 21:44:34 -0500

    get ip and port from command line input

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import sys
 from device import Device
 
-#device = Device('http://10.0.0.157:49153', '/setup.xml')
-device = Device('http://10.0.0.141:9197', '/dmr')
+if len(sys.argv) < 4:
+    print("Usage: main.py <ip> <port> <description_file_name>")
+    sys.exit(0)
+
+# TODO: sanitize input
+ip = sys.argv[1]
+port = sys.argv[2]
+desc = '/' + sys.argv[3]
+host = 'http://' + ip + ':' + port
+
+device = Device(host, desc)
+#device = Device('http://10.0.0.141:9197', '/dmr')
